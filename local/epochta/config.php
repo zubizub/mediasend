d108c1090a9f7d399b889aac712a3d801157fe06
root <root@p415432.kvmvps>
2018-01-18 05:27:01 +0300

    local

--- local/epochta/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'APISMS.php';
-//public & private ключи, можно установить на странице https://atompark.com/members/settings#sms
-$sms_key_private='5c884eaf74e14981449593775c1df487';
-$sms_key_public='1c7b283b007679c12b4cf4c5d43a0188';
-$URL_GAREWAY='http://atompark.com/api/sms/';
-$testMode=false;
